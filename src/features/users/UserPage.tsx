d5a6914a372b818d15222386b3fa16ab1f3ccf51
Vinayaka Iyer <vinayakaiyer999@gmail.com>
2025-02-05 15:11:31 +0530

    Transforming response using: transformResponse, selectFromResult and UseMemo

--- src/features/users/UserPage.tsx
+++ src/features/users/UserPage.tsx
@@ -4,12 +4,33 @@ import { useAppSelector } from '@/app/hooks'
 import { selectPostsByUser } from '../posts/postsSlice'
 
 import { selectUserById } from './usersSlice'
+import { createSelector } from '@reduxjs/toolkit'
+import type { TypedUseQueryStateResult } from '@reduxjs/toolkit/query/react'
+
+import { useGetPostsQuery, Post, useGetPostQuery } from '../api/apiSlice'
+// TS type that represents "the result value passed into the `selectFromResult` function for this hook"
+type GetPostSelectFromResultArg = TypedUseQueryStateResult<Post[], any, any>
+
+const selectPostsForUser = createSelector(
+  (res: GetPostSelectFromResultArg) => res.data,
+  (res: GetPostSelectFromResultArg, userId: string) => userId,
+  (data, userId) => data?.filter((post) => post.user === userId),
+)
 
 export const UserPage = () => {
   const { userId } = useParams()
 
   const user = useAppSelector((state) => selectUserById(state, userId!))
-  const postsForUser = useAppSelector((state) => selectPostsByUser(state, userId!))
+
+  // Use the same posts query, but extract only part of its data
+  const { postsForUser } = useGetPostsQuery(undefined, {
+    selectFromResult: (result) => ({
+      ...result,
+      //By calling selectPostsForUser(result, userId) every time, it will memoize
+      // the filtered array and only recalculate it if the fetched data or the user ID changes.
+      postsForUser: selectPostsForUser(result, userId!),
+    }),
+  })
 
   if (!user) {
     return (
@@ -19,7 +40,7 @@ export const UserPage = () => {
     )
   }
 
-  const postTitles = postsForUser.map((post) => (
+  const postTitles = postsForUser?.map((post) => (
     <li key={post.id}>
       <Link to={`/posts/${post.id}`}>{post.title}</Link>
     </li>
--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, createEntityAdapter, createSelector } from '@reduxjs/toolkit'
+import { createSlice, createEntityAdapter, createSelector, EntityState } from '@reduxjs/toolkit'
 import { RootState } from '@/app/store'
 import { selectCurrentUsername } from '@/features/auth/authSlice'
 import { client } from '@/api/client'
@@ -10,12 +10,17 @@ export interface User {
   name: string
 }
 
-const emptyUsers: User[] = []
+const usersAdapter = createEntityAdapter<User>()
+const initialState = usersAdapter.getInitialState()
 
 export const apiSliceWithUsers = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    getUsers: builder.query<User[], void>({
+    getUsers: builder.query<EntityState<User, string>, void>({
       query: () => '/users',
+      transformResponse(res: User[]) {
+        // Create a normalized state object containing all the user items
+        return usersAdapter.setAll(initialState, res)
+      },
     }),
   }),
 })
@@ -24,17 +29,14 @@ export const { useGetUsersQuery } = apiSliceWithUsers
 
 export const selectUsersResult = apiSliceWithUsers.endpoints.getUsers.select()
 
-export const selectAllUsers = createSelector(selectUsersResult, (usersResult) => usersResult?.data ?? emptyUsers)
-
-export const selectUserById = createSelector(
-  selectAllUsers,
-  (state: RootState, userId: string) => userId,
-  (users, userId) => users.find((user) => user.id === userId),
-)
+const selectUsersData = createSelector(selectUsersResult, (result) => result.data ?? initialState)
 
 export const selectCurrentUser = (state: RootState) => {
   const currentUsername = selectCurrentUsername(state)
-  if (currentUsername) {
-    return selectUserById(state, currentUsername)
+  if (!currentUsername) {
+    return
   }
+  return selectUserById(state, currentUsername)
 }
+
+export const { selectAll: selectAllUsers, selectById: selectUserById } = usersAdapter.getSelectors(selectUsersData)

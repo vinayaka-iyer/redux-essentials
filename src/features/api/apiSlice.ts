78039ff9c6198f08e8add204b7d34b8a78cded57
Vinayaka Iyer <vinayakaiyer999@gmail.com>
2025-02-05 12:09:13 +0530

    Automatic cache invalidation using 'Tags'

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -5,11 +5,15 @@ export type { Post }
 export const apiSlice = createApi({
   reducerPath: 'api', // optional
   baseQuery: fetchBaseQuery({ baseUrl: '/fakeApi' }),
+  // A root tagTypes field in the API slice object, declaring an array of string tag names for data types
+  tagTypes: ['Post'],
   endpoints: (builder) => ({
     // builder.query() accepts two generic arguments <ReturnType, ArgumentType>
     getPosts: builder.query<Post[], void>({
       // a query operation that returns Post[] array
       query: () => '/posts',
+      // A providesTags array in query endpoints, listing a set of tags describing the data in that query
+      providesTags: ['Post'],
     }),
     getPost: builder.query<Post, string>({
       query: (postId) => `/posts/${postId}`,
@@ -20,6 +24,8 @@ export const apiSlice = createApi({
         method: 'POST',
         body: initialPost,
       }),
+      // An invalidatesTags array in mutation endpoints, listing a set of tags that are invalidated every time that mutation runs
+      invalidatesTags: ['Post'],
     }),
   }),
 })
